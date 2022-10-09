621706300fd24ac0f4e166e4aacccbe3db8794e5
hzxdsg <3310087607@qq.com>
2022-10-09 19:51:10 +0800

    第一次提交

---
+++ create_id.py
@@ -0,0 +1,18 @@
+import random
+L1 = random.sample(range(1, 270), 90)
+"""
+L2 = random.sample(range(1, 270), 90)
+L3 = random.sample(range(1, 270), 90)
+L4 = random.sample(range(1, 270), 90)
+L5 = random.sample(range(1, 270), 90)
+"""
+print(L1,end="\n")
+"""
+print(L2,end="\n")
+print(L3,end="\n")
+print(L4,end="\n")
+print(L5,end="\n")
+"""
+absentNum=random.randint(5,8)
+absent1= random.sample(L1,absentNum)
+print(absent1,end="\n")
---
+++ main.py
@@ -0,0 +1,21 @@
+import pymysql
+# 连接数据库
+conn = pymysql.connect(host="localhost", user="root", password="123456",
+                       cursorclass=pymysql.cursors.DictCursor)
+try:
+    # 创建游标
+    cur = conn.cursor()
+    # 执行sql查询语句
+    cur.execute("CREATE DATABASE DS IF NOT EXISTS DS;")
+    cur.execute("USE DS;")
+    cur.execute("CREATE table if not EXISTS create table /"
+                "course(c_id int primary key,s_id varchar(9) ,/"
+                "stuName varchar(12),/"
+                "absentNumber int default 0);")
+    # 关闭游标
+    cur.close()
+    # 关闭数据库连接
+    conn.close()
+except pymysql.err.MySQLError as _error:
+    print('操作失败')
+    raise _error
