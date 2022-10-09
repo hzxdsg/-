fed827f1b7cb1d435cb3db09b02f556daaf8fca1
hzxdsg <3310087607@qq.com>
2022-10-09 19:51:10 +0800

    生成缺课名单

---
+++ create_id.py
@@ -0,0 +1,15 @@
+import random
+L1 = random.sample(range(1, 271), 90)     # 生成学号
+"""
+L2 = random.sample(range(1, 271), 90)
+L3 = random.sample(range(1, 271), 90)
+L4 = random.sample(range(1, 271), 90)
+L5 = random.sample(range(1, 271), 90)
+"""
+print(L1, end="\n")
+"""
+print(L2, end="\n")
+print(L3, end="\n")
+print(L4, end="\n")
+print(L5, end="\n")
+"""
